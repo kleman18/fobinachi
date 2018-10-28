4c614fa5a5f25c77ca3076b68d0755299e7a08a1
Volodymyr Kleman <v.kleman1996@gmail.com>
2018-10-28 20:34:10 +0000

    Initial

--- js/index.js
+++ js/index.js
@@ -1,8 +1,21 @@
-const n = parseInt(prompt('Введіть порядковий номер числа Фібоначчі'));
+let n = parseInt(prompt('Введіть порядковий номер числа Фібоначчі'));
 
 let first = 1;
 let second = 1;
 let next = 0;
+
+while (n <= 0) {
+    n = parseInt(prompt('Введіть ціле додатне число'));
+}
+
+// якщо використовувати parseFloat замість parseInt, то можна використовувати ще наступний цикл
+//while (n % 1 !== 0) {
+//    n = parseInt(prompt('Введіть ціле додатне число'));
+//}
+
+while (Number.isNaN(n)) {
+    n = parseFloat(prompt('Введіть ціле додатне число'));
+}
 
 if (n === 1) {
     alert(first);
@@ -18,5 +31,5 @@ else {
         first = second;
         second = next;
     }
-    alert (next);
 }
+alert(next);
